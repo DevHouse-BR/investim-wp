5521ed91cd8c3b770a1459167e83584ead4942e4
Leonardo Lima de Vasconcellos <leonardo@devhouse.com.br>
2017-06-02 00:03:17 -0300

    wpcasa-investim plugin

---
+++ www/wp-content/plugins/wpcasa-investim/wpcasa-investim.php
@@ -0,0 +1,357 @@
+<?php
+/**
+ * @package WPCasa-Investim
+ */
+/*
+Plugin Name: WPCasa Investim - Customização
+Plugin URI: https://devhouse.com.br/
+Description: Plugin desenvolvido especificamente para o site investim.com.br fornecendo customizações para a plataforma WPCasa.
+Version: 1.0.0
+Author: Leonardo Lima de Vasconcellos
+Author URI: https://devhouse.com.br/sobre/
+License: GPLv2 or later
+Text Domain: wpcasa-investim
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
+*/
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+/**
+ *	load_plugin_textdomain()
+ *	
+ *	Set up the text domain for the plugin
+ *	and load language files.
+ *	
+ *	@uses	plugin_basename()
+ *	@uses	load_plugin_textdomain()
+ *	
+ *	@since 1.0.0
+ */
+load_plugin_textdomain( 'wpcasa-investim', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+
+/**
+ * Detalhes de anúncio customizados para a Investim
+ */
+add_filter( 'wpsight_details', 'wpsight_details_custom' );
+function wpsight_details_custom( $details ) {
+
+	$details = array(
+		'razao_social' => array(
+			'id'			=> 'razao_social',
+			'label'			=> __( 'Razão Social', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'razao_social',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 10
+		),
+		'cnpj' => array(
+			'id'			=> 'cnpj',
+			'label'			=> __( 'CNPJ', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'cnpj',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 20
+		),
+		'constituicao' => array(
+			'id'			=> 'constituicao',
+			'label'			=> __( 'Constituição', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'data'			=> array( 
+				'' 			=> 'Selecione', 
+				'simples' 	=> 'Sociedade Simples', 
+				'limitada' 	=> 'Sociedade Limitada', 
+				'anonima' 	=> 'Sociedade Anônima'
+			),
+			'description'	=> '',
+			'query_var'		=> 'constituicao',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 30
+		),
+		'tributacao' => array(
+			'id'			=> 'tributacao',
+			'label'			=> __( 'Tributação', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'data'			=> array( 
+				''			=> 'Selecione', 
+				'simples'	=> 'Simples', 
+				'presumido' => 'Lucro Presumido', 
+				'real' 		=> 'Lucro Real'
+			),
+			'description'	=> '',
+			'query_var'		=> 'tributacao',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 40
+		),
+		'ativos' => array(
+			'id'			=> 'ativos',
+			'label'			=> __( 'Lista de Ativos e Equipamentos', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'ativos',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 50
+		),
+		'valor_estoque' => array(
+			'id'			=> 'valor_estoque',
+			'label'			=> __( 'Valor do Estoque a Preço de Custo', 'wpcasa-investim' ),
+			'unit'			=> 'R$',
+			'description'	=> '',
+			'query_var'		=> 'valor_estoque',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 60
+		),
+		'produtos' => array(
+			'id'			=> 'produtos',
+			'label'			=> __( 'Linha de Produtos', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'produtos',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 70
+		),
+		'servicos' => array(
+			'id'			=> 'servicos',
+			'label'			=> __( 'Linha de Serviços', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'servicos',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 80
+		),
+		'fundacao' => array(
+			'id'			=> 'fundacao',
+			'label'			=> __( 'Ano de Fundação', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'fundacao',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 90
+		),
+		'motivo_venda' => array(
+			'id'			=> 'motivo_venda',
+			'label'			=> __( 'Motivo da Venda', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'motivo_venda',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 100
+		),
+		'vol_vendas_a_1' => array(
+			'id'			=> 'vol_vendas_a_1',
+			'label'			=> __( 'Volume de Vendas Anuais (último ano)', 'wpcasa-investim' ),
+			'unit'			=> 'R$',
+			'description'	=> '(3 últimos anos)',
+			'query_var'		=> 'vol_vendas_a_1',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 110
+		),
+		'vol_vendas_a_2' => array(
+			'id'			=> 'vol_vendas_a_2',
+			'label'			=> __( 'Volume de Vendas Anuais (penúltimo ano)', 'wpcasa-investim' ),
+			'unit'			=> 'R$',
+			'description'	=> '(3 últimos anos)',
+			'query_var'		=> 'vol_vendas_a_2',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 120
+		),
+		'vol_vendas_a_3' => array(
+			'id'			=> 'vol_vendas_a_3',
+			'label'			=> __( 'Volume de Vendas Anuais (antepenúltimo ano)', 'wpcasa-investim' ),
+			'unit'			=> 'R$',
+			'description'	=> '(3 últimos anos)',
+			'query_var'		=> 'vol_vendas_a_3',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 130
+		),
+		'faturamento_mensal' => array(
+			'id'			=> 'faturamento_mensal',
+			'label'			=> __( 'Faturamento Mensal Médio', 'wpcasa-investim' ),
+			'unit'			=> 'R$',
+			'description'	=> '',
+			'query_var'		=> 'faturamento_mensal',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 140
+		),
+		'lucro_bruto' => array(
+			'id'			=> 'lucro_bruto',
+			'label'			=> __( 'Lucro Bruto', 'wpcasa-investim' ),
+			'unit'			=> 'R$',
+			'description'	=> '',
+			'query_var'		=> 'lucro_bruto',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 150
+		),
+		'margem_lucro' => array(
+			'id'			=> 'margem_lucro',
+			'label'			=> __( 'Margem de Lucro', 'wpcasa-investim' ),
+			'unit'			=> '%',
+			'description'	=> '',
+			'query_var'		=> 'margem_lucro',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 160
+		),
+		'lucro_liquido' => array(
+			'id'			=> 'lucro_liquido',
+			'label'			=> __( 'Lucro Líquido Médio', 'wpcasa-investim' ),
+			'unit'			=> 'R$',
+			'description'	=> '',
+			'query_var'		=> 'lucro_liquido',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 170
+		),
+		'imovel' => array(
+			'id'			=> 'imovel',
+			'label'			=> __( 'Imóvel', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'data'			=> array( 
+				'proprio' 	=> 'Próprio', 
+				'alugado' 	=> 'Alugado'
+			),
+			'description'	=> '',
+			'query_var'		=> 'imovel',
+			'data_compare'	=> '=',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 180
+		),
+		'endividamento' => array(
+			'id'			=> 'endividamento',
+			'label'			=> __( 'Endividamento', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'endividamento',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 190
+		),
+		'condicoes_venda' => array(
+			'id'			=> 'condicoes_venda',
+			'label'			=> __( 'Condições de Venda ou de Participação na Empresa', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'condicoes_venda',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 200
+		),
+		'numero_funcionarios' => array(
+			'id'			=> 'numero_funcionarios',
+			'label'			=> __( 'Número de Funcionários', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'numero_funcionarios',
+			'data_compare'	=> '>=',
+			'data_type'		=> 'numeric',
+			'dashboard'		=> true,
+			'position'		=> 210
+		),
+		'diferencial' => array(
+			'id'			=> 'diferencial',
+			'label'			=> __( 'Diferencial do Negócio', 'wpcasa-investim' ),
+			'unit'			=> '',
+			'description'	=> '',
+			'query_var'		=> 'diferencial',
+			'data_compare'	=> 'LIKE',
+			'data_type'		=> 'CHAR',
+			'dashboard'		=> true,
+			'position'		=> 220
+		),
+	);
+
+	return $details;
+ 
+}
+
+
+/**
+ * Customização de metaboxes
+ */
+add_filter( 'wpsight_meta_box_listing_details', 'wpsight_meta_box_listing_details_custom' );
+function wpsight_meta_box_listing_details_custom( $meta_box ) {
+
+	$meta_box['fields']['ativos']['type'] = 'textarea';
+	$meta_box['fields']['produtos']['type'] = 'textarea';
+	$meta_box['fields']['servicos']['type'] = 'textarea';
+	$meta_box['fields']['motivo_venda']['type'] = 'textarea';
+	$meta_box['fields']['imovel']['type'] = 'radio';
+	$meta_box['fields']['endividamento']['type'] = 'textarea';
+	$meta_box['fields']['condicoes_venda']['type'] = 'textarea';
+	$meta_box['fields']['diferencial']['type'] = 'textarea';
+
+	return $meta_box;
+
+}
+
+
+/**
+ * Customização de medidas
+ */
+add_filter( 'wpsight_measurements', 'wpsight_measurements_custom' );
+function wpsight_measurements_custom( $measurements ) {
+
+	$measurements['R$'] = 'R$';
+	$measurements['%'] = '%';
+
+	return $measurements;
+
+}
